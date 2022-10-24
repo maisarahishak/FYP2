66d19af5bd355e4453d9dd79fa862b0613e668d9
maisarahishak <maisarah2299@gmail.com>
2022-10-24 18:55:28 +0800

    setup

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
-  static String apiKey = "AIzaSyADZ6Uq-pFQIWMdkGeh7JIozEY7jJM6r_I";
-  static String messagingSenderId = "634480644405";
-  static String projectId = "chatappflutter-13318";
+  static String appId = "1:323306445180:web:1d6b8e8491873b66da2cf9";
+  static String apiKey = "AIzaSyAXUAaFiGd0ncQLOZhoH73LIJE61NNMM8E";
+  static String messagingSenderId = "323306445180";
+  static String projectId = "chatappflutter-581fd";
   final primaryColor = const Color(0xFFee7b64);
 }
